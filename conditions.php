15ec9a737798a59c7b9b493daf388926a5060cf4
Takowski <93830775+Takowski@users.noreply.github.com>
2023-10-19 15:18:49 +0200

    exercice 5

--- conditions.php
+++ conditions.php
@@ -109,6 +109,71 @@ if (isset($_GET['age']) && isset($_GET['gender'])) {
     }
     echo $message;
 }
+echo "<p>5. Display a different greeting according to the user's age, gender and mothertongue.</p>";
+
+echo '<form method="get" action="">
+    <label for="age">Enter your age:</label>
+    <input type="number" name="age" id="age"><br>
+    <label for="gender">Select your gender:</label>
+    <input type="radio" name="gender" value="male" id="male"><label for="male">Male</label>
+    <input type="radio" name="gender" value="female" id="female"><label for="female">Female</label><br>
+    <label for="english">Do you speak English?</label>
+    <input type="radio" name="english" value="yes" id="yes"><label for="yes">Yes</label>
+    <input type="radio" name="english" value="no" id="no"><label for="no">No</label><br>
+    <input type="submit" name="submit" value="Greet me now">
+</form>';
+
+if (isset($_GET['age']) && isset($_GET['gender']) && isset($_GET['english'])) {
+    $age = $_GET['age'];
+    $gender = $_GET['gender'];
+    $english = $_GET['english'];
+    if ($age < 12) {
+        if ($english == "yes") {
+            if ($gender == "female") {
+                $message = "Hello Girl!";
+            } else {
+                $message = "Hello boy!";
+            }
+        } else {
+            if ($gender == "female") {
+                $message = "Aloha Girl!";
+            } else {
+                $message = "Aloha boy!";
+            }
+        }
+    } elseif ($age >= 12 && $age <= 18) {
+        if ($english == "yes") {
+            if ($gender == "female") {
+                $message = "Hello Miss Teen!";
+            } else {
+                $message = "Hello mister Teen!";
+            }
+        } else {
+            if ($gender == "female") {
+                $message = "Aloha Miss Teen!";
+            } else {
+                $message = "Aloha mister Teen!";
+            }
+        }
+    } elseif ($age > 18 && $age <= 115) {
+        if ($english == "yes") {
+            if ($gender == "female") {
+                $message = "Hello Madam!";
+            } else {
+                $message = "Hello Sir!";
+            }
+        } else {
+            if ($gender == "female") {
+                $message = "Aloha Madam!";
+            } else {
+                $message = "Aloha Sir!";
+            }
+        }
+    } else {
+        $message = "Wow! Still alive? Are you a fucking elf?";
+    }
+    echo $message;
+}
 ?>  
 </body>
 </html>
