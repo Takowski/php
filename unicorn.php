5148081c60db5bfbf32d119d606f7f29ee3e5cbe
Takowski <93830775+Takowski@users.noreply.github.com>
2023-10-20 15:41:47 +0200

    unicorn finish

--- unicorn.php
+++ unicorn.php
@@ -1,23 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ternary</title>
 </head>
+
 <body>
-<?php
-echo "<p>11. Ternary exercice - unicorn variant</p>";
-echo '<form method="get" action="">
-<input type="radio" name="gender" value="M" id="male"><label for="male">Male</label>
-    <input type="radio" name="gender" value="F" id="female"><label for="female">Female</label><br>
+    <?php
+    echo "<p>11. Ternary exercice - unicorn variant</p>";
+    echo '<form method="get" action="">
+    <input type="radio" name="gender" value="M" id="male"><label for="male">Male</label>
+    <input type="radio" name="gender" value="F" id="female"><label for="Female">Female</label><br>
+    <input type="radio" name="gender" value="U" id="unicorn"><label for="Unicorn">Unicorn</label><br>
     <input type="submit" name="submit" value="Greeting"> 
 </form>';
+    $hello = (isset($_GET['gender']) && $_GET['gender'] === "M") ? "Hello sir!" : ((isset($_GET['gender']) && $_GET['gender'] === "F") ? "Hello madam!" : "Hello unicorn!");
+    echo $hello;
 
-$hello = (isset($_GET['gender']) && $_GET['gender'] === "M") ? "Hello sir!" : "Hello madam!";
-echo $hello;
 
-
-?>
+    ?>
 </body>
+
 </html>
