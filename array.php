197369b44cf7dad5078188eb7bb54253dddba357
Takowski <93830775+Takowski@users.noreply.github.com>
2023-10-23 13:36:08 +0200

    array

---
+++ array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>array</title>
+</head>
+<body>
+ <?php
+$me= array (
+    "name" => "Stephane",
+    "age" => 41,
+    "country" => "Belgium",
+    "city" => "Mons",
+    "hobbies" => array("nothing", "nothing more", "Nothing else"),
+);
+?>   
+</body>
+</html>
